2e6d6d0ef568315758f71e76a08ba330c3e3002e
aarvee <rvs.rv11@gmail.com>
2016-12-10 12:28:46 +0530

    Create system-monitor.sh

---
+++ system-monitor.sh
@@ -0,0 +1,18 @@
+#/bin/bash!
+# Author: aarvee11
+# Use: system monitor script to dump CPU, RAM and HDD Usage on an ubuntu 14.04 box in total used %
+
+MYIP=$(hostname -I);
+MYHOSTNAME=$(hostname)
+ID=$(curl http://169.254.169.254/latest/meta-data/instance-id);
+while true
+do
+	FREE_DATA=`free -m | grep Mem` 
+	CURRENT=`echo $FREE_DATA | cut -f3 -d' '`
+	TOTAL=`echo $FREE_DATA | cut -f2 -d' '`
+	echo "{\"time\":\""$(date +%Y-%m-%dT%H:%M:%S%z)"\",\"AWS-ID\":\""$ID"\",\"CPU-LOAD\":`top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`,\"MEMORY-USED\":$(echo "scale = 2; $CURRENT/$TOTAL*100" | bc),\"DISK-USED\":`df -lh | awk '{if ($6 == "/") { print $5 }}' | head -1 | cut -d'%' -f1`}" >> /var/log/system-health.log;
+	unset FREE_DATA;
+	unset CURRENT;
+	unset TOTAL;
+	sleep 10;
+done
